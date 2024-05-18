a3e8509a3c47fffecf4669b4b895a797099afb54
joaothallys <joaothallys>
2024-05-17 22:37:15 -0300

    cors3

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const router = express.Router();
-const mysql = require('mysql');
 const cors = require('cors');
+const mysql = require('mysql');
+const router = express.Router();
 
-
-const corsOptions = {
-    origin: 'https://node-deploy-api-d20r.onrender.com',
-    methods: ['GET', 'POST' , 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-};
-
+// Importar as rotas
 const motoristaRoutes = require('./routes/motoristaRoutes.js');
 const fornecedoresRoutes = require('./routes/fornecedoresRoutes.js');
 const produtosRoutes = require('./routes/produtosRoutes.js');
@@ -28,12 +22,20 @@ const pedido_produtoRoutes = require('./routes/pedido_produtoRoutes.js');
 const permissaoRoutes = require('./routes/permissaoRoutes.js');
 const login = require('./validators/login.js');
 
-
-
 const app = express();
+
+// Configurar o CORS para permitir acesso de todas as origens
+const corsOptions = {
+    origin: '*', // Permitir todas as origens
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
+app.use(express.json());
 
-
+// Configuração do banco de dados MySQL
 const db = mysql.createConnection({
     host: "monorail.proxy.rlwy.net",
     port: 19967,
@@ -50,7 +52,7 @@ db.connect((err) => {
     }
 });
 
-
+// Usar as rotas
 app.use('/', motoristaRoutes(db));
 app.use('/', fornecedoresRoutes(db));
 app.use('/', produtosRoutes(db));
@@ -68,13 +70,7 @@ app.use('/', pedido_produtoRoutes(db));
 app.use('/', permissaoRoutes(db));
 app.use('/', login(db));
 
-
-app.use('/', router);
-app.use(cors());
-app.use(express.json());
-app.use('/api', login(db));
-app.use(cors(corsOptions));
-
+// Iniciar o servidor
 const PORT = process.env.PORT || 3300;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
