6db7e7f5e8c3e824a93ade0e14d75cc333e7adaa
Larissa Moreira <larissamoreiracarvalho@gmail.com>
2024-05-21 18:46:04 -0300

    feat: add react

---
+++ build/precache-manifest.0af8c368cf15b69547dcfdc2a900f406.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59838277966fe33c677910603e5bffbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fdfda011cb11acbe179",
+    "url": "/static/css/main.4a461f84.chunk.css"
+  },
+  {
+    "revision": "a01450c473d54061eb28",
+    "url": "/static/js/2.9ddcae1e.chunk.js"
+  },
+  {
+    "revision": "97b5938ef990e58844f0cff8514cc372",
+    "url": "/static/js/2.9ddcae1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fdfda011cb11acbe179",
+    "url": "/static/js/main.6d6154f6.chunk.js"
+  },
+  {
+    "revision": "31fe52e0bb68f83722f1",
+    "url": "/static/js/runtime-main.7094e3bb.js"
+  },
+  {
+    "revision": "cf3282008cd6008a6917ce43624a1076",
+    "url": "/static/media/home.cf328200.svg"
+  },
+  {
+    "revision": "848da6b85a1e5ec8379e52e27e00cc73",
+    "url": "/static/media/logogrande.848da6b8.png"
+  },
+  {
+    "revision": "060a268b966e6c66afb228e68bf53735",
+    "url": "/static/media/logopequena.060a268b.png"
+  },
+  {
+    "revision": "e344aebfc67c59e4d8a35b3b98664569",
+    "url": "/static/media/logout.e344aebf.svg"
+  },
+  {
+    "revision": "0832f1b1a0b7fd047dc81d1c919802c5",
+    "url": "/static/media/monitoramento.0832f1b1.svg"
+  },
+  {
+    "revision": "317463668ed183385a09834ae9ec422e",
+    "url": "/static/media/torre-controle.31746366.svg"
+  },
+  {
+    "revision": "1950dc320f648af96f349ef81168a487",
+    "url": "/static/media/transportadora.1950dc32.svg"
+  },
+  {
+    "revision": "b9af6547730c4dd7216d9d2efee239e8",
+    "url": "/static/media/usuario.b9af6547.svg"
+  }
+]);
