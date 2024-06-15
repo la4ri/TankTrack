29be91a575b83195d94523749c50e5564a3631f0
Larissa Moreira <larissamoreiracarvalho@gmail.com>
2024-06-15 20:32:18 -0300

    feat/estilização aba disponibilidade de Veículos

--- src/components/monitoramento/DispVeiculosPage.js
+++ src/components/monitoramento/DispVeiculosPage.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Select, MenuItem } from '@mui/material';
 import axios from 'axios';
+import { Select, MenuItem } from '@mui/material';
+import '../../style/container.css';
+import '../../style/menu.css';
 
 const DispVeiculosPage = () => {
     const [veiculos, setVeiculos] = useState([]);
@@ -25,47 +27,39 @@ const DispVeiculosPage = () => {
 
     return (
         <div>
-            <div className="containerMonitoramento">
-                <div className="content">
-                    <div className="main-section">
-                        <Container>
-                            <Typography variant="h4" gutterBottom>
-                                Disponibilidade de Veículos
-                            </Typography>
-                            <TableContainer component={Paper} style={{ marginTop: '20px' }}>
-                                <Table>
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell>ID Veículo</TableCell>
-                                            <TableCell>Placa</TableCell>
-                                            <TableCell>Status</TableCell>
-                                            <TableCell>Apto para Viagem</TableCell>
-                                            <TableCell>Disponível para Viagem</TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {veiculos.map((veiculo) => (
-                                            <TableRow key={veiculo.id_veiculo}>
-                                                <TableCell>{veiculo.id_veiculo}</TableCell>
-                                                <TableCell>{veiculo.placa_veiculo}</TableCell>
-                                                <TableCell>{veiculo.status_veiculo}</TableCell>
-                                                <TableCell>{veiculo.status_veiculo === 'Disponível' ? 'Sim' : 'Não'}</TableCell>
-                                                <TableCell>
-                                                    <Select
-                                                        value={disponibilidade[veiculo.id_veiculo] || 'Sim'}
-                                                        onChange={(e) => handleDisponibilidadeChange(veiculo.id_veiculo, e.target.value)}
-                                                    >
-                                                        <MenuItem value="Sim">Sim</MenuItem>
-                                                        <MenuItem value="Não">Não</MenuItem>
-                                                    </Select>
-                                                </TableCell>
-                                            </TableRow>
-                                        ))}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                        </Container>
-                    </div>
+            <div className="main-content">
+                <h1>Disponibilidade de Veículos</h1>
+                <div className="table-container">
+                    <table className="product-table">
+                        <thead>
+                            <tr>
+                                <th>ID Veículo</th>
+                                <th>Placa</th>
+                                <th>Status</th>
+                                <th>Apto para Viagem</th>
+                                <th>Disponível para Viagem</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {veiculos.map((veiculo) => (
+                                <tr key={veiculo.id_veiculo}>
+                                    <td>{veiculo.id_veiculo}</td>
+                                    <td>{veiculo.placa_veiculo}</td>
+                                    <td>{veiculo.status_veiculo}</td>
+                                    <td>{veiculo.status_veiculo === 'Disponível' ? 'Sim' : 'Não'}</td>
+                                    <td>
+                                        <Select
+                                            value={disponibilidade[veiculo.id_veiculo] || 'Sim'}
+                                            onChange={(e) => handleDisponibilidadeChange(veiculo.id_veiculo, e.target.value)}
+                                        >
+                                            <MenuItem value="Sim">Sim</MenuItem>
+                                            <MenuItem value="Não">Não</MenuItem>
+                                        </Select>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
